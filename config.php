d2123fce3a5bf4b15fee52d79a2e34bd86a9907e
dorcasgicuku <dorcausgicuku@gmail.com>
2017-06-15 15:50:39 +0100

    add customer

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php 
-include("lib/db.class.php");
+
  $base= "pos";
  $server= "localhost";
  $user= "root"; 
  $cpass= "dorcas1994";
- $db = new DB($base, $server, $user, $pass);
+ 
 //$db = new DB("pos", "localhost", "root", "dorcas1994");
 
 ?>
